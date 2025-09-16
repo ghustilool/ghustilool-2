112b623ae1f8fe21b0a2bc4c8885da13d0579ba5
ghustilool <162752783+ghustilool@users.noreply.github.com>
2025-09-16 18:32:06 -0300

    Update cargarPublicaciones.js

--- scripts/cargarPublicaciones.js
+++ scripts/cargarPublicaciones.js
@@ -1,19 +1,27 @@
-async function cargarPublicaciones(autor) {
-  const res = await fetch(`data/${autor}.json`);
-  const data = await res.json();
-
+function cargarPublicaciones(autor) {
   const contenedor = document.getElementById('publicaciones');
   contenedor.innerHTML = '';
 
-  data.publicaciones.forEach(pub => {
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML = `
-      <img src="${pub.imagen}" alt="${pub.titulo}">
-      <h3>${pub.titulo}</h3>
-      <p>${pub.descripcion}</p>
-      <a href="${pub.link}" target="_blank">Descargar</a>
-    `;
-    contenedor.appendChild(div);
-  });
+  fetch(`autores/${autor}.json`)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(juego => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.setAttribute('data-tags', juego.tags.join(','));
+
+        card.innerHTML = `
+          <img src="${juego.imagen}" alt="${juego.nombre}">
+          <h3>${juego.nombre}</h3>
+          <p>${juego.descripcion}</p>
+          <a href="${juego.instalador}">Instalar</a>
+        `;
+
+        contenedor.appendChild(card);
+      });
+    })
+    .catch(err => {
+      contenedor.innerHTML = `<p style="color:#ff0033;">No se pudo cargar el autor "${autor}".</p>`;
+      console.error(`Error al cargar ${autor}.json`, err);
+    });
 }
