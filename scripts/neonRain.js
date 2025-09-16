fafc45e53faf68718393a839177bf7cb46178793
ghustilool <162752783+ghustilool@users.noreply.github.com>
2025-09-16 17:04:54 -0300

    Add files via upload

--- scripts/neonRain.js
+++ scripts/neonRain.js
@@ -4,21 +4,56 @@ script.src = "https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js";
 script.onload = () => {
   particlesJS("neon-background", {
     "particles": {
-      "number": { "value": 80 },
-      "color": { "value": "#00ffcc" },
-      "shape": { "type": "circle" },
-      "opacity": { "value": 0.5 },
-      "size": { "value": 3 },
+      "number": {
+        "value": 120,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#00ffcc"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.6,
+        "random": true,
+        "anim": {
+          "enable": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": false
+        }
+      },
+      "line_linked": {
+        "enable": false
+      },
       "move": {
         "enable": true,
-        "speed": 2,
+        "speed": 3,
         "direction": "bottom",
-        "out_mode": "out"
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false
       }
     },
     "interactivity": {
       "detect_on": "canvas",
-      "events": { "onhover": { "enable": false } }
+      "events": {
+        "onhover": {
+          "enable": false
+        },
+        "onclick": {
+          "enable": false
+        },
+        "resize": true
+      }
     },
     "retina_detect": true
   });
